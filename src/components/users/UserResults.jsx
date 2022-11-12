c5f827e7c33e419fe6fc14f1a8873f825c3d171a
prakash naikwadi <naikwadiprakash1@gmail.com>
2022-11-12 17:00:13 +0530

    GITHUB API added to fetch data from github, spinner component added

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 function UserResults() {
-  return <div>UserResults</div>;
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    setUsers(data);
+    setIsLoading(false);
+  };
+
+  if (!isLoading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <h3>
+        <Spinner />
+      </h3>
+    );
+  }
 }
 
 export default UserResults;
